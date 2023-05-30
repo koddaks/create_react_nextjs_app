dcacde00648c90d63527c409cb082feee7e2cebd
koddaks <koss199710@gmail.com>
2023-05-30 13:35:11 +0300

    create react-nextjs-app

---
+++ pages/posts/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Posts() {
+  return (
+    <div>
+      <h1>Posts:</h1>
+      <h1>
+        <Link href="/">Back to home</Link>
+      </h1>
+    </div>
+  );
+}
